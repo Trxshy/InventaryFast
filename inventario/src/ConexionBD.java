9c592dcb8b34504547124dcb1824c0bfb2826c59
AlexisM <alexis.m16.99@gmail.com>
2020-05-14 16:54:52 -0300

    Hello

---
+++ inventario/src/ConexionBD.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+public class ConexionBD {
+    
+    public static Connection conexion() throws SQLException{
+        
+            String url ="jdbc:mysql://168.138.149.15:3306/mydb";
+            String user = "inventary_fast";
+            String pass = "InventaryFast__//1";
+            Connection cn = DriverManager.getConnection(url,user,pass);  
+            System.out.println("Conexion Exitosa");
+            try {
+            return cn;
+        } catch (Exception e) {
+            return cn;
+        }
+     
+    }
+    public static ResultSet getTabla(String consulta) throws SQLException{
+        Connection cn = conexion();
+        Statement st;
+        ResultSet datos = null;
+        try {
+            st = cn.createStatement();
+            datos= st.executeQuery(consulta);
+        } catch (Exception e) {
+            
+        }
+        
+        return datos;
+    }
+    
+    public static void main(String[] args) {
+
+                
+    }
+    
+}
